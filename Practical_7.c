651f62c79d0a7d184c268ab862bc937bc9e3cb8a
shrey276 <shrey369h@gmail.com>
2024-09-25 23:16:24 +0530

    prime oky !!

--- Practical_7.c
+++ Practical_7.c
@@ -1,13 +1,34 @@
 /********** ["To check whether the given number is prime or not"] **********/
 #include <stdio.h>
-int main()
-{
-    int i,n;
-    printf("enter the number :\n");
-    scanf("%d",&n);
 
-    
-return 0;
-    
-        
+int main() {
+    int num, i, P = 1;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        P = 0;
+    }
+    else if (num > 1)
+    {
+        for (i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                P = 0; 
+                break;
+            }
+        }
+    }
+
+    if(P)
+    {
+        printf("%d is a prime number.\n", num);
+    }
+
+    else
+    {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
 }
