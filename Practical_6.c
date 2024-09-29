0f4f7569e4d40783a867418e6342e0cd092c8aa8
shrey276 <shrey369h@gmail.com>
2024-09-29 19:50:44 +0530

    Practical_6 update

--- Practical_6.c
+++ Practical_6.c
@@ -1,81 +1,40 @@
-/********** ["Program to display 3 students' student name, roll number and marks of 4subjects and also display the total marks and percentage of each student"] **********/
-
-
 #include <stdio.h>
 int main(){
 
     char s1[15],s2[15],s3[15];
     char *s[] = {s1,s2,s3};
-    int i;
-
-    printf("\n\n====================================================================================== \n\n");
+    int i,j,r[3];
     for(i = 0; i <= 2; i++)
     {
         printf ("enter student%d name : ",i+1);
         scanf ("%s",s[i]);
-
-    }
-    
-    
-    int j,r[3];
-
-    printf("\n\n====================================================================================== \n\n");
-    for(i = 0; i <= 2; i++)
-    {
         printf ("enter %s's roll no : ",s[i]);
         scanf ("%d",&r[i]);
-    
     }
-
 
     int LDP[3],PEP[3],AEM[3],CEP[3];
     int *sub[] = {LDP,PEP,AEM,CEP};
     char *subname[] = {"LDP","PEP","AEM","CEP"};
 
-    printf("\n\n====================================================================================== \n");
-    printf("\"Enter the marks out of 30\"");
-    printf("\n====================================================================================== \n");
-    for(i = 0; i <= 2; i++)
-    {
-        printf("\n-*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *- \n");
-
-        for(j = 0; j <= 3; j++)
-        {
+    printf("\n\"Enter the marks out of 30\"\n");
+    for(i = 0; i <= 2; i++){
+        printf("\n");
+        for(j = 0; j <= 3; j++){
             printf ("enter %s marks of roll no %d : ",subname[j],r[i]);
             scanf ("%d",&sub[j][i]);
         } 
     }
 
-    printf("\n\n\n ========= *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* ======== \n\n");
-
-// RESULT 
-   
-    int LDPm[3],PEPm[3],AEMm[3],CEPm[3];
-    int *subm[] = {LDPm,PEPm,AEMm,CEPm};
     int sm[3] ;
-
-    for(i = 0; i <= 2; i++)
-    {
-        for(j = 0; j <= 3; j++)
-        {
-            subm[j][i] = (sub[j][i] * 100)/30;
-        }
-        sm[i] =  (LDPm[i] + PEPm[i] + AEMm[i] + CEPm[i]) / 4 ; 
+    int percentage[3] ;
+    for(i = 0; i <= 2; i++){
+        sm[i] =  (LDP[i] + PEP[i] + AEM[i] + CEP[i]) ;
+        percentage[i] = ((sm[i]*100)/120); 
     }
-
-// PRINT RESULT
-
-    printf ("|------------------------------------------------------------------------|\n");
-    printf ("|| Roll no. |      Name:      |  LDP%% |  PEP%% |  AEM%% |  CEP%% |  FINAL%% ||\n");
-    printf ("||----------|-----------------|-------|-------|-------|-------|---------||\n");
-
-    for(i = 0; i <= 2; i++)
-    { 
-        printf ("|| %-7d. | %-15s | %-5d | %-5d | %-5d | %-5d | %-7d ||\n",&r[i],s[i],LDPm[i],PEPm[i],AEMm[i],CEPm[i],sm[i]);  
+    
+    printf ("\n\n| Roll no. |      Name:      | LDP | PEP | AEM | CEP | TOTAL | Per%% |\n");
+    printf ("|----------|-----------------|-----|-----|-----|-----|-------|------|\n");
+    for(i = 0; i <= 2; i++){ 
+        printf ("| %-7d. | %-15s | %-3d | %-3d | %-3d | %-3d | %-5d | %-4d%%|\n",r[i],s[i],LDP[i],PEP[i],AEM[i],CEP[i],sm[i],percentage[i]);  
     }
-
-    printf ("|------------------------------------------------------------------------|\n");
-
-
-
 }
